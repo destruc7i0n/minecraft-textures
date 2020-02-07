948e1e3aaf541a3d7b5688cf8a342b6de1788687
destruc7i0n <dscdsouza@outlook.com>
2020-02-07 12:54:39 -0500

    new dynamic exports

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Minecraft Textures
 This is a library containing all the textures I use for my generators. You're free to use it so long as you credit me.
 
-### There are 1.12 to 1.15 textures currently available.
+### There are 1.12 to 1.16 textures currently available.
 
 The Minecraft item icons are owned by Mojang AB.
 This is not endorsed by Mojang AB.
 
 `npm install minecraft-textures`
+
+#### Notes
+- When upgrading to 1.16, the versions are now dynamically imported.
+    - Sorry to anyone who used the .json files, but this makes the packages much smaller, for faster first load times :(
 
 ### Created by TheDestruc7i0n
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 /* TheDestruc7i0n 2020 */
 
 const textures = {
-    '1.12': require('./textures/112').default,
-    '1.13': require('./textures/113').default,
-    '1.14': require('./textures/114').default,
-    '1.15': require('./textures/115').default,
-    '1.16': require('./textures/116').default,
+    '1.12': '112',
+    '1.13': '113',
+    '1.14': '114',
+    '1.15': '115',
+    '1.16': '116',
 }
 
-const byVersion = (version) => {
+const byVersion = async (version) => {
     // cast to string if needed
     version = String(version)
     // check if the texture exists and return accordingly
-    return textures.hasOwnProperty(version) ?
-        textures[version] :
-        {}
+    if (textures.hasOwnProperty(version)) {
+        // return a promise to the file
+        const { default: textures } = await import(`./textures/${textures[version]}`)
+        return textures
+    }
+    return {}
 }
 
 export const versions = Object.keys(textures)
