ea1e2c3af15d5f646c2579558ff3e4196097f19a
destruc7i0n <dscdsouza@outlook.com>
2020-05-12 22:19:27 -0400

    small fixes

--- index.ts
+++ index.ts
@@ -1,23 +1,31 @@
 /* TheDestruc7i0n 2020 */
 
-const textures: { [key: string]: string } = {
-    '1.12': '112',
-    '1.13': '113',
-    '1.14': '114',
-    '1.15': '115',
-    '1.16': '116',
+interface Textures {
+  comment: string,
+  items: {
+    readable: string
+    id: string
+    texture: string
+  }[]
 }
 
-const byVersion = async (version: string) => {
-    // cast to string if needed
-    version = String(version)
-    // check if the texture exists and return accordingly
-    if (textures.hasOwnProperty(version)) {
-        // return a promise to the file
-        const { default: tex } = await import(`./textures/${textures[version]}`)
-        return tex
-    }
-    return {}
+const textures: { [key: string]: string } = {
+  '1.12': '112',
+  '1.13': '113',
+  '1.14': '114',
+  '1.15': '115',
+  '1.16': '116',
+}
+
+const byVersion = async (version: string): Promise<Textures | undefined> => {
+  // cast to string if needed
+  version = String(version)
+  // check if the texture exists and return accordingly
+  if (textures.hasOwnProperty(version)) {
+    // return a promise to the file
+    const { default: tex } = await import(`./textures/${textures[version]}`)
+    return tex
+  }
 }
 
 export const versions = Object.keys(textures)
