f5c5f870d9eee3a321fe028312e8fe24f84e62f7
destruc7i0n <destruc7i0n@users.noreply.github.com>
2023-04-11 13:10:50 -0400

    update to use misode/mcmeta in list missing script

--- scripts/listMissing.ts
+++ scripts/listMissing.ts
@@ -6,33 +6,25 @@ import { latestVersion } from '../index';
 
 // latest list of items from the reports from Arcensoth
 const VERSION_FILE =
-  'https://github.com/Arcensoth/mcdata/raw/master/VERSION.txt';
+  'https://github.com/misode/mcmeta/raw/registries/version.json';
 const ITEMS_FILE =
-  'https://github.com/Arcensoth/mcdata/raw/master/processed/reports/registries/item/data.json';
-
-interface Registry {
-  values: string[];
-}
+  'https://github.com/misode/mcmeta/raw/registries/item/data.json'; // no minecraft: prefix
 
 // lists all the missing items, probably from a new version
 const main = async () => {
-  const remoteLatestVersion = await (await fetch(VERSION_FILE)).text();
+  const remoteLatestVersion = (await (await fetch(VERSION_FILE)).json()).id;
   console.log(
     `Comparing (local) items from ${latestVersion} to (remote) ${remoteLatestVersion}`
   );
 
-  const { values: allItems } = (await (
-    await fetch(ITEMS_FILE)
-  ).json()) as Registry;
+  const allItems = (await (await fetch(ITEMS_FILE)).json()) as string[];
 
   const latest: TexturesType = (await import(`../textures/${latestVersion}.ts`))
     .default;
-  const ids = latest.items.map((i) => i.id);
+  const ids = latest.items.map((i) => i.id.replace('minecraft:', ''));
 
   // compare files and exclude air
-  const diff = allItems.filter(
-    (v) => !ids.includes(v) && v !== 'minecraft:air'
-  );
+  const diff = allItems.filter((v) => !ids.includes(v) && v !== 'air');
 
   console.log(`Found ${diff.length} missing items.`);
 
